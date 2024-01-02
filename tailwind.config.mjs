dcd05ec4526a9f1b8c3df309695d71d9a2a82933
lexandfr <alexfloresrayme@gmail.com>
2024-01-01 21:24:55 -0500

    Update tailwind.config.mjs with new theme colors and fonts

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,29 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    colors: {
+      podia: "#fdfbf9",
+      white: "#ffffff",
+      "podia-primary": {
+        light: "#9484c6",
+        DEFAULT: "#4b2aad",
+        dark: "#201445",
+      },
+      "podia-secondary": "#9c8881",
+      "podia-shadow": "#f5efeb",
+      "podia-description": "#5c5856",
+    },
+    extend: {
+      fontFamily: {
+        headland: "Headland One",
+        "zen-antique": "Zen Antique",
+      },
+    },
+  },
+  plugins: [require("daisyui")],
+
+  daisyui: {
+    darkTheme: "light",
+  },
+};
